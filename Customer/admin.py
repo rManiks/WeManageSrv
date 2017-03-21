f5997a26c37ace23ce2a06f4ebb86d13fca9d262
Ramachandran <Manikandan.Ramachandran@CAPCO.COM>
2017-03-21 14:21:29 +0000

    Initial models added

--- Customer/admin.py
+++ Customer/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Address)
+admin.site.register(Party)
+admin.site.register(Property)
--- Customer/models.py
+++ Customer/models.py
@@ -1,10 +1,36 @@
 from django.db import models
 
+#Address Model
+class Address(models.Model):
+    name = models.CharField(max_length=50)
+    line_1 = models.CharField(max_length=100)
+    line_2 = models.CharField(max_length=100, null=True)
+    line_3 = models.CharField(max_length=100, null=True)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=50)
+    post_code = models.CharField(max_length=10, default='N/A')
+    misc_info = models.CharField(max_length=200)
+    geo_location = models.IntegerField
+
+    def __str__(self):
+        return self.name
+
+
+#Party Model
+class Party(models.Model):
+    name = models.CharField(max_length=100)
+    address_id = models.ForeignKey(Address, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 #Property Model
-class Property(modesl.Model):
-    id = models.Integer
+class Property(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=250)
-    address_id = models.Integer
-    owner_id = models.Integer
+    address_id = models.ForeignKey(Address, on_delete=models.CASCADE)
+    owner_id = models.ForeignKey(Party)
     
+    def __str__(self):
+        return self.name
